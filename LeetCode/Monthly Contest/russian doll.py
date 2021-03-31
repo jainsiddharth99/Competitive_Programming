e47115616bc0e1295b02ecc51413f1858e237e19
jainsiddharth99 <groupofsjain@gmail.com>
2021-03-31 15:59:32 +0530

    #354

--- LeetCode/Monthly Contest/russian doll.py
+++ LeetCode/Monthly Contest/russian doll.py
@@ -1,12 +1,13 @@
 def maxEnvelopes(envelopes):
     envelopes=sorted(envelopes)
-    print(envelopes)
+    # print(envelopes)
     ans=[]
-    for envelope in envelopes:
-        print(envelope)
-        for i in range(1,len(envelope),2):
-            # print(i)
-            print(envelopes[i])
+    for i in range(len(envelopes)):
+        ans.append(envelopes[0])
+        for j in range(i+1,len(envelopes)):
+            if ans[-1][0]< envelopes[j][0] and ans[-1][1]< envelopes[j][1]:
+                ans.append(envelopes[j])
+        return ans
             
 env=[[5,4],[6,4],[6,7],[2,3]]
 print(maxEnvelopes(env))
