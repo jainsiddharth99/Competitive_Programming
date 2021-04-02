e0244367de9c749c58535e41e1f5273cc227c2bf
jainsiddharth99 <groupofsjain@gmail.com>
2021-04-02 15:17:06 +0530

    #234

--- LeetCode/palindrome linked list.py
+++ LeetCode/palindrome linked list.py
@@ -1,3 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def isPalindrome(self, head: ListNode) -> bool:
+#         slow = fast = head 
+#         while fast != None and fast.next != None:
+#             slow = slow.next 
+#             fast = fast.next.next
+#         if fast != None:  # case number of elements is odd, move mid to next element.
+#             slow = slow.next
+        
+#         def reverse(head):
+#             ans = None
+#             while head != None:
+#                 next = head.next
+#                 head.next = ans
+#                 ans = head
+#                 head = next
+#             return ans
+        
+#         head2 = reverse(slow) # slow is our mid
+#         while head2 != None:
+#             if head.val != head2.val:
+#                 return False
+#             head = head.next
+#             head2 = head2.next
+#         return True
+        
+
 def isPalindrome(x):
     x=str(x)
     print(x)
