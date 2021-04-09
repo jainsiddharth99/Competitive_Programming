efc01c170b40fe9144104b8516a169094d548664
jainsiddharth99 <groupofsjain@gmail.com>
2021-04-09 14:48:23 +0530

    #953

---
+++ LeetCode/Verifying an Alien Dictionary.py
@@ -0,0 +1,17 @@
+def isAlienSorted(words,order):
+    order_map = {}
+    for index, val in enumerate(order):
+        order_map[val] = index
+        
+    for i in range(len(words)-1):
+        for j in range(len(words[i])):
+            if j>=len(words[i+1]):
+                return False
+            if words[i][j]!=words[i+1][j]:
+                if order_map[words[i][j]]>order_map[words[i+1][j]]:
+                    return False
+                break
+    return True
+words=["apple","app"]
+order="abcdefghijklmnopqrstuvwxyz"
+print(isAlienSorted(words,order))
