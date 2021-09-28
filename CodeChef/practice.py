81aa8bb32ea26954f410df5c3b20639b4da8c994
jainsiddharth99 <groupofsjain@gmail.com>
2021-09-28 14:48:10 +0530

    completed

--- CodeChef/practice.py
+++ CodeChef/practice.py
@@ -3,11 +3,11 @@
 # for i in range(n):
 #     v=int(input())
 #     val.append(v)
-val=[10,5,15,10,20,9,21]
+val=[10,5,15,10,20,9,21,21]
 ans=[]
 for i in range(len(val)):
     print(i)
-    if val[i]==val[0]:
+    if val[i]==val[0] and i==1:
         pass
     elif val[i]==val[-1]:
         pass
