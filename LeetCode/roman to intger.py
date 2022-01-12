7dc0308e3dc15087441ccfa4242c87a22b6e03c0
jainsiddharth99 <groupofsjain@gmail.com>
2022-01-12 17:47:20 +0530

    #13

---
+++ LeetCode/roman to intger.py
@@ -0,0 +1,17 @@
+roman={'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+s=input()
+ans=0
+for i in range(0,len(s)-1):
+    if roman[s[i]]<roman[s[i+1]]:
+        ans-=roman[s[i]]
+    else:
+        ans+=roman[s[i]]
+        
+ans+=roman[s[-1]]
+print(ans)
+        
+    
+    
+    
+
+            
