5f55c61dd517a0d7edbd60f94385f4e0489fc506
jainsiddharth99 <groupofsjain@gmail.com>
2022-01-21 17:17:51 +0530

    #14

---
+++ LeetCode/longest common prefix.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        prefix = ""
+        if len(strs) == 1:
+            return strs[0]
+        for i in range(len(strs[0])):
+            prefix += strs[0][i]
+            for j in range(1,len(strs)):
+                if strs[j].startswith(prefix):
+                    continue
+                else:
+                    return prefix[:-1]
+        if prefix != "":
+            return prefix
+        return ""
