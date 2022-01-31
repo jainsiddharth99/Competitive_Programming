1c16bbf3e207fe250c4367dbe9f77d978971eea9
jainsiddharth99 <groupofsjain@gmail.com>
2022-01-31 11:34:13 +0530

    #1672

--- LeetCode/Richest Customer Wealth.py
+++ LeetCode/Richest Customer Wealth.py
@@ -4,3 +4,12 @@ for i in accounts:
     a=sum(i)
     l.append(a)
 print (max(l))
+
+# class Solution:
+#     def maximumWealth(self, accounts: List[List[int]]) -> int:
+#         t=[]
+#         for i in accounts:
+#             a=sum(i)
+#             t.append(a)
+#         return max(t)
+        
