4aa345c644fe19882c7d8f3592a8db6e38e312cf
jainsiddharth99 <groupofsjain@gmail.com>
2022-02-02 13:36:17 +0530

    #438

--- LeetCode/Find All Anagrams in a String.py
+++ LeetCode/Find All Anagrams in a String.py
@@ -21,13 +21,13 @@ while len(p)>=len(t)+1 and i<=len(s)-1:
 print(t)
 print(l)    
     
-class Solution:
-    def findAnagrams(self, s: str, p: str) -> List[int]:
-    	LS, LP, S, P, A = len(s), len(p), 0, 0, []
-    	if LP > LS: return []
-    	for i in range(LP): S, P = S + hash(s[i]), P + hash(p[i])
-    	if S == P: A.append(0)
-    	for i in range(LP, LS):
-    		S += hash(s[i]) - hash(s[i-LP])
-    		if S == P: A.append(i-LP+1)
-    	return A
+# class Solution:
+#     def findAnagrams(self, s: str, p: str) -> List[int]:
+#     	LS, LP, S, P, A = len(s), len(p), 0, 0, []
+#     	if LP > LS: return []
+#     	for i in range(LP): S, P = S + hash(s[i]), P + hash(p[i])
+#     	if S == P: A.append(0)
+#     	for i in range(LP, LS):
+#     		S += hash(s[i]) - hash(s[i-LP])
+#     		if S == P: A.append(i-LP+1)
+#     	return A
