ba37b5fd4336e5de27b57347cc47b0c0f821be44
jainsiddharth99 <groupofsjain@gmail.com>
2022-03-13 11:40:41 +0530

    #20

---
+++ LeetCode/Valid Parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        b=['(','[','{']
+        dic={'(':')','[':']','{':'}'}
+        lt=[]
+        for i in s:
+            if i in dic:
+                lt.append(dic[i])
+            else:
+                if len(lt)>0 and lt[-1]==i:
+                    lt.pop()
+                else:
+                    return False
+        return len(lt)==0
+        
