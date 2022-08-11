dffe984ac4c9ed38013c1d3a61bd3df1f77fdfc2
jainsiddharth99 <groupofsjain@gmail.com>
2022-08-11 17:56:38 +0530

    inteviewbit problem

--- LeetCode/Concatenation of Array.py
+++ LeetCode/Concatenation of Array.py
@@ -1,6 +1,17 @@
 def getConcatenation( nums: list[int]) -> list[int]:
     return nums*2
 
-nums = [1,2,1]
-ans=getConcatenation(nums)
+def solve(A):
+    res=[]
+    for i in range(len(A)):
+        for j in range(len(res)):
+            if res[j]==A[i]:
+                res[j]+=1
+                res.append(A[i])
+            else:
+                res.append(A[i])
+    return res
+nums = [1,2]
+# ans=getConcatenation(nums)
+ans= solve(nums)
 print(ans)
