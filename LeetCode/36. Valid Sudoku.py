ac71c5b46abfc2d8b1aab72425dee8fbe1cd4976
jainsiddharth99 <groupofsjain@gmail.com>
2022-08-20 15:32:31 +0530

    logic buliding

--- LeetCode/36. Valid Sudoku.py
+++ LeetCode/36. Valid Sudoku.py
@@ -8,14 +8,27 @@ def isValidSudoku(board):
         box[i]={}
     for i in range(1,10):
         for j in range(1,10):
-            row[i][j]=None
-            column[i][j]=None
-            box[i][j]=None
-
-    for i in range(0,9):
-        for j in range(0,9):
-            column[i+1][j+1]=board[j][i]
+            row[i][j]=0
+            column[i][j]=0
+            box[i][j]=0
+            
+    #print (row)
+    for i in range(9):
+        for j in range(9):
+            cur=board[i][j]
+            if cur !='.':
+                cur=int(cur)
+                # for row
+                if cur in row[i+1] and row[i+1][cur]==0:
+                    row[i+1][cur]+=1
+                    # for column
+                if cur in column[j+1] and column[j+1][cur]==0:
+                    column[j+1][cur]+=1
+            else:
+                pass
+                
     return column
+                
 
 
 
