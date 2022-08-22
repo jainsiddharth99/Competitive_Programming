39eb9f652235dd42b5f128585045784361b57a42
jainsiddharth99 <groupofsjain@gmail.com>
2022-08-22 17:36:03 +0530

    #977

---
+++ LeetCode/977. Squares of a Sorted Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        res=[]
+        for i in nums:
+            res.append(i*i)
+            
+        res.sort()
+        return res
