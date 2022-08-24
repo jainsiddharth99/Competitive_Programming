81744a3211ce24d1be1788002c7ea1a1031e9158
jainsiddharth99 <groupofsjain@gmail.com>
2022-08-24 19:04:09 +0530

    48

---
+++ LeetCode/48. Rotate Image.py
@@ -0,0 +1,10 @@
+def rotate(matrix):
+    matrix.reverse()
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+rotate(matrix)
