505451c86d8d198a4c80ffc76bb4fa78fe386a3f
jainsiddharth99 <groupofsjain@gmail.com>
2022-08-26 17:37:45 +0530

    best solution

--- LeetCode/41. First Missing Positive.py
+++ LeetCode/41. First Missing Positive.py
@@ -1,5 +1,35 @@
 def firstMissingPositive(nums):
     # * edge cases
+    if 1 not in nums:
+        return 1
+    high = max(nums)
+    low = min(nums)
+
+    if high < 1:
+        return 1
+    if low > 1:
+        return 1
+    if high == 1:
+        return 2
+
+    # * main logic
+
+    n = len(nums)
+    for i in range(n):
+        if nums[i] <= 0 or nums[i] > n:
+            nums[i] = n+1
+    for i in range(n):
+        temp = abs(nums[i]) - 1
+        if temp < n and nums[temp] > 0:
+            nums[temp] *= -1
+
+    for i in range(n):
+        if nums[i] > 0:
+            return i+1
+
+    else:
+        return n+1
 
 
-nums = [3, 0, 1]
+nums = [1, 2, 0]
+print(firstMissingPositive(nums))
--- LeetCode/448. Find All Numbers Disappeared in an Array.py
+++ LeetCode/448. Find All Numbers Disappeared in an Array.py
@@ -1,25 +1,25 @@
 def findDisappearedNumbers(nums):
     # * first approach
-    # n = len(nums)
-    # for i in range(n):
-    #     temp = abs(nums[i])-1
-    #     if nums[temp] > 0:
-    #         nums[temp] *= -1
-
-    # res = []
-    # for i in range(n):
-    #     if nums[i] > 0:
-    #         res.append(i+1)
-    # return res
-
     n = len(nums)
-    s = set(nums)
+    for i in range(n):
+        temp = abs(nums[i])-1
+        if nums[temp] > 0:
+            nums[temp] *= -1
+
     res = []
-    for i in range(1, n+1):
-        if i not in s:
-            res.append(i)
+    for i in range(n):
+        if nums[i] > 0:
+            res.append(i+1)
     return res
 
+    # n = len(nums)
+    # s = set(nums)
+    # res = []
+    # for i in range(1, n+1):
+    #     if i not in s:
+    #         res.append(i)
+    # return res
 
-nums = [4, 3, 2, 7, 8, 2, 3, 1]
+
+nums = [3, 4, 0, 1]
 print(findDisappearedNumbers(nums))
