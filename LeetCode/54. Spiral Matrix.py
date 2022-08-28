77e6232cd4c0b10f93a63b918f907af1f4173639
jainsiddharth99 <groupofsjain@gmail.com>
2022-08-28 19:08:32 +0530

    Solved

--- LeetCode/54. Spiral Matrix.py
+++ LeetCode/54. Spiral Matrix.py
@@ -1,4 +1,4 @@
-def spiral(matrx):
+def spiral(matrix):
     '''
     Edge cases:
 
@@ -7,40 +7,45 @@ def spiral(matrx):
     '''
     res = []
     m, n = len(matrix), len(matrix[0])
-    if n % 2 == 0:
-        ran = n//2
-    else:
-        ran = (n//2)+1
-    # first_row,last_row,first_col,last_col=matrix[0],matrix[-1],
+    a = m*n
+    if m == 1:
+        return matrix[0]
 
-    first_row, first_col, last_row, last_col = 0, 0, m-1, n-1
-    i, j = 0, 0
-    while ran > 0:
-        # first row
-        for i in range(first_row, last_col):
+    first_row, first_col = 0, 0
+    while first_row < n and first_col < m:
+        # First Row
+
+        for i in range(first_row, n):
             res += [matrix[first_row][i]]
-            first_row += 1
+        first_row += 1
 
-        # last col
-        for j in range(first_row, last_row):
-            res += [matrix[j][last_col]]
-            last_col -= 1
+        if len(res) == a:
+            break
+
+        # Last column
+
+        for j in range(first_row, m):
+            res += [matrix[j][n-1]]
+        if len(res) == a:
+            break
+
+        n -= 1
         # last row
+        for k in range(n-1, first_col, -1):
+            res += [matrix[m-1][k]]
+        if len(res) == a:
+            break
 
-        for k in range(last_col, first_col, -1):
-            res += [matrix[last][k]]
-
-    # for i in matrix[0]:
-    #     res += [i]
-    # for i in range(1, m):
-    #     res += [matrix[i][-1]]
-    # for i in range(m-2, -1, -1):
-    #     res += [matrix[-1][i]]
-    # for i in range(n-2, 0, -1):
-    #     res += [matrix[i][0]]
+        m -= 1
+        # first col
+        for l in range(m, first_col, -1):
+            res += [matrix[l][first_col]]
+        first_col += 1
+        if len(res) == a:
+            break
 
     return res
 
 
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+matrix = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]
 print(spiral(matrix))
