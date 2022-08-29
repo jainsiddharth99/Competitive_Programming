f023c6aa8ba515bffa75c8f2ea45b5027bf7a140
jainsiddharth99 <groupofsjain@gmail.com>
2022-08-29 11:59:55 +0530

    Better

--- LeetCode/54. Spiral Matrix.py
+++ LeetCode/54. Spiral Matrix.py
@@ -5,47 +5,87 @@ def spiral(matrix):
     1. As first will be the first elements always we can add
     them initially
     '''
+    # res = []
+    # m, n = len(matrix), len(matrix[0])
+    # a = m*n
+
+    # first_row, first_col = 0, 0
+    # while first_row < n and first_col < m:
+    #     # First Row
+
+    #     for i in range(first_row, n):
+    #         res += [matrix[first_row][i]]
+    #     first_row += 1
+
+    #     if len(res) == a:
+    #         break
+
+    #     # Last column
+
+    #     for j in range(first_row, m):
+    #         res += [matrix[j][n-1]]
+    #     if len(res) == a:
+    #         break
+
+    #     n -= 1
+    #     # last row
+    #     for k in range(n-1, first_col, -1):
+    #         res += [matrix[m-1][k]]
+    #     if len(res) == a:
+    #         break
+
+    #     m -= 1
+    #     # first col
+    #     for l in range(m, first_col, -1):
+    #         res += [matrix[l][first_col]]
+    #     first_col += 1
+    #     if len(res) == a:
+    #         break
+
+    # return res
+
+    # 2nt try
+
+    start_row = 0
+    start_col = 0
+    end_row = len(matrix)-1
+    end_col = len(matrix[0])-1
+    total = len(matrix)*len(matrix[0])
+    count = 0
     res = []
-    m, n = len(matrix), len(matrix[0])
-    a = m*n
-    if m == 1:
-        return matrix[0]
+    while count < total:
 
-    first_row, first_col = 0, 0
-    while first_row < n and first_col < m:
-        # First Row
-
-        for i in range(first_row, n):
-            res += [matrix[first_row][i]]
-        first_row += 1
-
-        if len(res) == a:
+        for i in range(start_row, end_col+1):
+            res.append(matrix[start_row][i])
+            count += 1
+        if count == total:
             break
 
-        # Last column
+        start_row += 1
 
-        for j in range(first_row, m):
-            res += [matrix[j][n-1]]
-        if len(res) == a:
+        for i in range(start_row, end_row+1):
+            res.append(matrix[i][end_col])
+            count += 1
+        if count == total:
             break
+        end_col -= 1
 
-        n -= 1
-        # last row
-        for k in range(n-1, first_col, -1):
-            res += [matrix[m-1][k]]
-        if len(res) == a:
+        for i in range(end_col, start_col-1, -1):
+            res.append(matrix[end_row][i])
+            count += 1
+        if count == total:
             break
+        end_row -= 1
 
-        m -= 1
-        # first col
-        for l in range(m, first_col, -1):
-            res += [matrix[l][first_col]]
-        first_col += 1
-        if len(res) == a:
+        for i in range(end_row, start_row-1, -1):
+            res.append(matrix[i][start_col])
+            count += 1
+        if count == total:
             break
+        start_col += 1
 
     return res
 
 
-matrix = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]
+matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
 print(spiral(matrix))
