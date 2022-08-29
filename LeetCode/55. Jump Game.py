efc7fb3dc3422bfe1c88f5a04306f5e31bc590fe
jainsiddharth99 <groupofsjain@gmail.com>
2022-08-29 19:03:16 +0530

    both

--- LeetCode/55. Jump Game.py
+++ LeetCode/55. Jump Game.py
@@ -1,5 +1,11 @@
 def canjump(nums):
     # edge case
+    # target=len(nums) - 1
+    # for i in range(len(nums)-1,-1,-1):
+    #     if i+nums[target] >=target:
+    #         target=i
+    # return target==0
+
     j = 0
     for i, num in enumerate(nums):
         if i > j:
@@ -9,5 +15,5 @@ def canjump(nums):
     return True
 
 
-nums = [0, 1]
+nums = [0]
 print(canjump(nums))
