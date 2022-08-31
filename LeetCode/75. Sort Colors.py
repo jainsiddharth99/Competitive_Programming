3ce5dc1070f77a880b75a8e9336ab5a22c0e8f28
jainsiddharth99 <groupofsjain@gmail.com>
2022-08-31 16:38:23 +0530

    75

---
+++ LeetCode/75. Sort Colors.py
@@ -0,0 +1,24 @@
+def sortColors(self, nums: list[int]) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+
+    print(nums)
+
+
+nums = [2, 0, 2, 1, 1, 0]
+sortColors(nums)
+
+
+"""
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+"""
