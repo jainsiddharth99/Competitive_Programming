71303347b88cdfe1870d70114f6f155e0ce0e122
jainsiddharth99 <groupofsjain@gmail.com>
2022-08-31 13:01:58 +0530

    solved

---
+++ LeetCode/122. Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,26 @@
+def maxprofit(prices):
+    curr_profit = 0
+    profit = 0
+    buy = prices[0]
+
+    for i in prices:
+        curr_profit = i-buy
+        if curr_profit > 0:
+            buy = i
+            profit += curr_profit
+        buy = min(buy, i)
+
+    return profit
+
+
+prices = [7, 6, 4, 3, 1]
+print(maxprofit(prices))
+
+
+"""
+Input: 
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Total profit is 4 + 3 = 7.
+"""
