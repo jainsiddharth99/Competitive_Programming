0a25721672e0bef2ccb14e109e9d2f742d3e882e
jainsiddharth99 <groupofsjain@gmail.com>
2022-08-31 17:58:52 +0530

    done

--- LeetCode/75. Sort Colors.py
+++ LeetCode/75. Sort Colors.py
@@ -1,7 +1,22 @@
-def sortColors(self, nums: list[int]) -> None:
+def sortColors(nums: list[int]) -> None:
     """
     Do not return anything, modify nums in-place instead.
     """
+    n = len(nums)
+    red = 0
+    white = 0
+    blue = n-1
+
+    while white <= blue:
+        if nums[white] == 0:
+            nums[white], nums[red] = nums[red], nums[white]
+            white += 1
+            red += 1
+        elif nums[white] == 1:
+            white += 1
+        else:
+            nums[white], nums[blue] = nums[blue], nums[white]
+            blue -= 1
 
     print(nums)
 
