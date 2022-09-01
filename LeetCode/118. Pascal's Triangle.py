a9c69c71ad0a9da4679e336e297a5bfbbdaa7fc6
jainsiddharth99 <groupofsjain@gmail.com>
2022-09-01 10:27:16 +0530

    solved

--- LeetCode/118. Pascal's Triangle.py
+++ LeetCode/118. Pascal's Triangle.py
@@ -1,21 +1,22 @@
 def generate(numRows):
     n = numRows
-
+    # Edge cases
     if n == 0:
         return []
     if n == 1:
         return [[1]]
     if n == 2:
         return [[1], [1, 1]]
+
+    # 2 initila values
     res = [[1], [1, 1]]
-
-    for i in range(2, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(res[i-1][j-1]+res[i-1][j])
-        row.append(1)
-        res.append(row)
-
+    # main logic
+    for i in range(2, n):  # starting with 2 since we dealt with some edge cases
+        curr = [1]
+        for j in range(1, i):  # starting
+            curr.append(res[-1][j-1]+res[-1][j])
+        curr.append(1)
+        res.append(curr)
     return res
 
 
