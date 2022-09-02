1b0c835f6e2303970e946e3b921675f16588f8fd
jainsiddharth99 <groupofsjain@gmail.com>
2022-09-02 19:20:09 +0530

    smaller method

--- LeetCode/84. Largest Rectangle in Histogram.py
+++ LeetCode/84. Largest Rectangle in Histogram.py
@@ -1,43 +1,59 @@
-def nextsmaller(heights, n):
-    stack = [-1]
-    next = [-1]*n
-    for i in range(n-1, -1, -1):
-        curr = heights[i]
-        while stack[-1] != -1 and heights[stack[-1]] > curr:
-            stack.pop()
+# def nextsmaller(heights, n):
+#     stack = [-1]
+#     next = [-1]*n
+#     for i in range(n-1, -1, -1):
+#         curr = heights[i]
+#         while stack[-1] != -1 and heights[stack[-1]] > curr:
+#             stack.pop()
 
-        next[i] = stack[-1]
-        stack.append(i)
-    return next
+#         next[i] = stack[-1]
+#         stack.append(i)
+#     return next
 
 
-def prevsmaller(heights, n):
-    stack = [-1]
-    prev = [-1]*n
-    for i in range(n):
-        curr = heights[i]
-        while stack[-1] != -1 and heights[stack[-1]] >= curr:
-            stack.pop()
+# def prevsmaller(heights, n):
+#     stack = [-1]
+#     prev = [-1]*n
+#     for i in range(n):
+#         curr = heights[i]
+#         while stack[-1] != -1 and heights[stack[-1]] >= curr:
+#             stack.pop()
 
-        prev[i] = stack[-1]
-        stack.append(i)
-    return prev
+#         prev[i] = stack[-1]
+#         stack.append(i)
+#     return prev
 
+
+# def largestRectangleArea(heights: list[int]) -> int:
+
+#     n = len(heights)
+#     next = nextsmaller(heights, n)
+#     prev = prevsmaller(heights, n)
+#     max_area = 0
+#     for i in range(n):
+#         h = heights[i]
+#         if next[i] == -1:
+#             next[i] = n
+#         w = next[i]-prev[i] - 1
+#         area = h*w
+#         max_area = max(area, max_area)
+#     return max_area
 
 def largestRectangleArea(heights: list[int]) -> int:
-
     n = len(heights)
-    next = nextsmaller(heights, n)
-    prev = prevsmaller(heights, n)
-    max_area = 0
-    for i in range(n):
-        h = heights[i]
-        if next[i] == -1:
-            next[i] = n
-        w = next[i]-prev[i] - 1
-        area = h*w
-        max_area = max(area, max_area)
-    return max_area
+    maxArea = 0
+    stack = []
+    for i, h in enumerate(heights):
+        start = i
+        while stack and stack[-1][1] > h:
+            index, height = stack.pop()
+            maxArea = max(maxArea, height*(i-index))
+            start = index
+        stack.append((start, h))
+
+    for i, h in stack:
+        maxArea = max(maxArea, h*(n-i))
+    return maxArea
 
 
 heights = [2, 1, 5, 6, 2, 3]
