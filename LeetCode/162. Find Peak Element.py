ff4e599c4bb35f9168d49c50423161e657e3a9c6
jainsiddharth99 <groupofsjain@gmail.com>
2022-09-06 17:04:26 +0530

    logn solution

--- LeetCode/162. Find Peak Element.py
+++ LeetCode/162. Find Peak Element.py
@@ -4,3 +4,15 @@ def findPeakElement(nums: list[int]) -> int:
         if nums[i] > nums[i+1]:
             return i
     return len(nums)-1
+# class Solution:
+#     def findPeakElement(self, nums: List[int]) -> int:
+#         return search(nums,0,len(nums)-1)
+
+
+# def search(nums,start,end):
+#     if start==end:
+#         return start
+#     mid=(start+end)//2
+#     if nums[mid]<nums[mid+1]:
+#         return search(nums,mid+1,end)
+#     return search(nums,start,mid)
