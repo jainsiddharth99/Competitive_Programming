6e41de47c551550b50759c7f198a2d9c0574b32a
jainsiddharth99 <groupofsjain@gmail.com>
2022-09-11 14:06:26 +0530

    solved

---
+++ LeetCode/404. Sum of Left Leaves.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        total = 0
+        if root is None:
+            return 0
+        if root.left:
+            total += root.left.val
+
+        total += self.sumOfLeftLeaves(root.left)
+        total += self.sumOfLeftLeaves(root.right)
+
+        return total
