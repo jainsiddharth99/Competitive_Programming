d54b779694f247f99ebd5e0e31339f2aa1ffbbf0
jainsiddharth99 <groupofsjain@gmail.com>
2022-09-13 12:36:53 +0530

    solved

--- LeetCode/24. Swap Nodes in Pairs.py
+++ LeetCode/24. Swap Nodes in Pairs.py
@@ -5,11 +5,18 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(None, head)
-        prev, val = dummy, head
-        while val and val.next:
-            prev.next = val.next
-            val.next = val.next.next
-            prev.next.next = val
-            prev, val = val, val.next
-        return dummy.next
+        if head != None and head.next != None:
+            head.val, head.next.val = head.next.val, head.val
+            self.swapPairs(head.next.next)
+        return head
+
+
+# class Solution:
+#     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         tmp=head
+#         if tmp is None:return
+#         while tmp and tmp.next:
+#             if tmp.val!=tmp.next.val:
+#                 tmp.val,tmp.next.val=tmp.next.val,tmp.val
+#             tmp=tmp.next.next
+#         return head
