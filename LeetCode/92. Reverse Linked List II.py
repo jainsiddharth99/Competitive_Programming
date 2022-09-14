9290a0eda10d4c83481d5a384457639bc55c863d
jainsiddharth99 <groupofsjain@gmail.com>
2022-09-14 15:50:34 +0530

    solved

--- LeetCode/92. Reverse Linked List II.py
+++ LeetCode/92. Reverse Linked List II.py
@@ -25,3 +25,29 @@ class Solution:
             head = prev
         tail.next = curr
         return head
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+#     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+
+#         if right<=left:
+#             return head
+#         dummy=ListNode(0)
+#         dummy.next=head
+
+#         pre = dummy
+#         curr=dummy.next
+
+#         for  i in range(1,left):
+#             pre=pre.next
+#             curr=curr.next
+
+#         for i in range(right-left):
+#             tmp=curr.next
+#             curr.next=tmp.next
+#             tmp.next=pre.next
+#             pre.next=tmp
+#         return dummy.next
