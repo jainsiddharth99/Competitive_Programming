75445ca81e7c87c7edc85d24472af1c14f62c18e
jainsiddharth99 <groupofsjain@gmail.com>
2022-09-18 12:19:47 +0530

    sosolvedlved

--- LeetCode/1171. Remove Zero Sum Consecutive Nodes from Linked List.py
+++ LeetCode/1171. Remove Zero Sum Consecutive Nodes from Linked List.py
@@ -1,18 +1,60 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        total = 0
+        dic = {0: dummy}
+        while head:
+            total += head.val
+            dic[total] = head
+            head = head.next
+
+        head = dummy
+        total = 0
+        while head:
+            total += head.val
+            head.next = dic[total].next
+            head = head.next
+
+        return dummy.next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return []
         lt = []
         c = head
         while c:
-            lt.append(c.val)
+            if c.val:
+                lt.append(c.val)
             c = c.next
         n = len(lt)
-        s = [lt[0]]
+        s = []
 
-        for i in range(1, n):
-            if s[-1]+lt[i] == 0:
-                s.pop()
+        for i in lt:
+            if s:
+                total = i
+                for j in range(len(s)-1, -1, -1):
+                    total += s[j]
+                    if total == 0:
+                        s = s[:j]
+                        break
+                else:
+                    s.append(i)
+
             else:
-                s.append(lt[i])
+                s.append(i)
+
         curr = dummy = ListNode(0)
         for i in s:
             curr.next = ListNode(i)
