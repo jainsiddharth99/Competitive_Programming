b24a4fc922629cfd6f461a4a49c9a5c1e18c47f7
jainsiddharth99 <groupofsjain@gmail.com>
2022-09-20 15:34:01 +0530

    solved

--- LeetCode/2074. Reverse Nodes in Even Length Groups.py
+++ LeetCode/2074. Reverse Nodes in Even Length Groups.py
@@ -1,8 +1,4 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+
 class Solution:
     def reverseEvenLengthGroups(self, head: Optional[ListNode]) -> Optional[ListNode]:
         curr = head
