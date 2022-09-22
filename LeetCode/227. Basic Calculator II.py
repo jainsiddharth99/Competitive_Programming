9aeba53e5f5a4ee875ab84fa183ca4a9527104e1
jainsiddharth99 <groupofsjain@gmail.com>
2022-09-22 15:16:21 +0530

    solved

---
+++ LeetCode/227. Basic Calculator II.py
@@ -0,0 +1,29 @@
+def calculate(s: str) -> int:
+    s = s.replace(" ", "")
+    s += '+'
+    st = []
+    num = 0
+    op = '+'
+    # num = ''
+    for i in s:
+        if i.isdigit():
+            num = (num*10)+int(i)
+            # num += i
+        else:
+            num = int(num)
+            if op == '+':
+                st.append(num)
+            elif op == '-':
+                st.append(-num)
+            elif op == '*':
+                st.append(st.pop()*num)
+            elif op == '/':
+                st.append(int(st.pop()/num))
+            # num = ''
+            num = 0
+            op = i
+    return sum(st)
+
+
+s = "14-3/2"
+print(calculate(s))
