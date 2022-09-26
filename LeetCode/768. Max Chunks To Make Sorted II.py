2120b7b1c098a4ff77339c541261b47454ca7c29
jainsiddharth99 <groupofsjain@gmail.com>
2022-09-26 17:06:46 +0530

    solved

---
+++ LeetCode/768. Max Chunks To Make Sorted II.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        s = []
+        for i in arr:
+            curr = i
+            while s and s[-1] > i:
+                curr = max(curr, s.pop())
+            s.append(curr)
+        return len(s)
