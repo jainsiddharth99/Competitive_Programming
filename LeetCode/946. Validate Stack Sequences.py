b0a308c4954c050da6ddd309d56c7f9ee0b9854f
jainsiddharth99 <groupofsjain@gmail.com>
2022-09-29 18:38:02 +0530

    solved

---
+++ LeetCode/946. Validate Stack Sequences.py
@@ -0,0 +1,30 @@
+def validateStackSequences(pushed: list[int], popped: list[int]) -> bool:
+    s = []
+    j = 0
+    for i in pushed:
+        s.append(i)
+        while s and s[-1] == popped[j]:
+            s.pop()
+            j += 1
+    return len(s) == 0
+
+
+pushed = [1, 2, 3, 4, 5]
+popped = [4, 5, 3, 2, 1]
+print(validateStackSequences(pushed, popped))
+"""
+Example 1:
+
+Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+Output: true
+Explanation: We might do the following sequence:
+push(1), push(2), push(3), push(4),
+pop() -> 4,
+push(5),
+pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+Example 2:
+
+Input: pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+Output: false
+Explanation: 1 cannot be popped before 2.
+"""
