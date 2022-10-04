a91ff9d135c89adc71b14774d1489e5ed29a6dd6
jainsiddharth99 <groupofsjain@gmail.com>
2022-10-04 18:43:20 +0530

    Solving

---
+++ LeetCode/862. Shortest Subarray with Sum at Least K.py
@@ -0,0 +1,19 @@
+def shortestSubarray(nums: list[int], k: int) -> int:
+
+    pass
+
+
+"""
+Example 1:
+
+Input: nums = [1], k = 1
+Output: 1
+Example 2:
+
+Input: nums = [1,2], k = 4
+Output: -1
+Example 3:
+
+Input: nums = [2,-1,2], k = 3
+Output: 3
+"""
