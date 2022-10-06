07f1be5dbebb2e6bd6a89412b75dec1adbfdad2e
jainsiddharth99 <groupofsjain@gmail.com>
2022-10-06 12:55:44 +0530

    solved

--- LeetCode/862. Shortest Subarray with Sum at Least K.py
+++ LeetCode/862. Shortest Subarray with Sum at Least K.py
@@ -1,7 +1,25 @@
+from collections import deque
+
+
 def shortestSubarray(nums: list[int], k: int) -> int:
+    pre = [0]
+    for i in nums:
+        pre.append(pre[-1]+i)
+    n = len(nums)
+    q = deque()
+    res = n+1
+    for i, val in enumerate(pre):
+        while q and val <= pre[q[-1]]:
+            q.pop()
+        while q and val-pre[q[0]] >= k:
+            res = min(res, i-q.popleft())
+        q.append(i)
 
-    pass
+    return res if res < n+1 else -1
 
+
+nums = [2, -1, 2]
+print(shortestSubarray(nums, 3))
 
 """
 Example 1:
