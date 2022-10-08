84b9ecaa54687a70c39898d9f84b5d561ae5d5a2
jainsiddharth99 <groupofsjain@gmail.com>
2022-10-08 16:52:19 +0530

    Solved

--- LeetCode/41. First Missing Positive.py
+++ LeetCode/41. First Missing Positive.py
@@ -1,18 +1,4 @@
 def firstMissingPositive(nums):
-    # * edge cases
-    if 1 not in nums:
-        return 1
-    high = max(nums)
-    low = min(nums)
-
-    if high < 1:
-        return 1
-    if low > 1:
-        return 1
-    if high == 1:
-        return 2
-
-    # * main logic
 
     n = len(nums)
     for i in range(n):
@@ -31,5 +17,43 @@ def firstMissingPositive(nums):
         return n+1
 
 
-nums = [1, 2, 0]
-print(firstMissingPositive(nums))
+def firstMissingPositive2(nums: list[int]) -> int:
+    for i, num in enumerate(nums):
+        if num <= 0:
+            nums[i] = 0
+
+    for i, num in enumerate(nums):
+        index = abs(num)-1
+        if index >= 0 and index < len(nums):
+            if nums[index] == 0:
+                nums[index] = -inf
+            elif nums[index] > 0:
+                nums[index] *= -1
+
+    for index, num in enumerate(nums):
+        if num >= 0:
+            return index + 1
+
+    return len(nums) + 1
+
+
+def firstMissingPositive3(nums):
+
+    n = len(nums)
+    i = 0
+    while i < n:
+        curr = nums[i]
+        if 1 <= curr <= n:
+            position = curr-1
+            if nums[position] != curr:
+                nums[i], nums[position] = nums[position], nums[i]
+                i -= 1
+        i += 1
+    for i in range(n):
+        if nums[i] != i+1:
+            return i+1
+    return n+1
+
+
+nums = [3, 4, -1, 1]
+print(firstMissingPositive3(nums))
