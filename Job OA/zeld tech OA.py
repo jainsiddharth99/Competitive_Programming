0909f0ed002eabb87eefde63c746bbb8fbfeba87
jainsiddharth99 <groupofsjain@gmail.com>
2022-10-10 13:05:45 +0530

    solved

--- Job OA/zeld tech OA.py
+++ Job OA/zeld tech OA.py
@@ -5,23 +5,15 @@ def solution(A):
         return 0
 
     for i in range(len(A)):
-        s = sub(A, 0, len(A)-1)
-        s = s[:i]+s[i+1:]
+        s = A[:i]+A[i+1:]
         if cutting(s):
             res += 1
     return res if res != 0 else -1
 
 
-def sub(A, start, end):
-    lt = []
-    for i in range(start, end+1):
-        lt.append(A[i])
-    return lt
-
-
 def cutting(A):
     for i in range(1, len(A)-1):
-        if (A[i-1] <= A[i] and A[i] <= A[i+1]) or (A[i-1] >= A[i] and A[i] >= A[i+1]):
+        if (A[i-1] <= A[i] <= A[i+1]) or (A[i-1] >= A[i] >= A[i+1]):
             return False
     return True
 
