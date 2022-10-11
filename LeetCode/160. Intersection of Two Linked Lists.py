e8fee21cdeaa3afc432821721a340d0f4d247dbf
jainsiddharth99 <groupofsjain@gmail.com>
2022-10-11 18:05:03 +0530

    solved

---
+++ LeetCode/160. Intersection of Two Linked Lists.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        curr = headA
+        ele = set()
+        while curr:
+            ele.add(curr)
+            curr = curr.next
+
+        curr = headB
+        while curr:
+            if curr in ele:
+                return curr
+            curr = curr.next
+
+        return None
--- LeetCode/36. Valid Sudoku.py
+++ LeetCode/36. Valid Sudoku.py
@@ -1,56 +1,60 @@
 def isValidSudoku(board):
     # * old solution
-    # row={}
-    # column={}
-    # box=[]
+    row = {}
+    column = {}
+    box = []
 
-    # for i in range(0, 9, 3):
-    #     for j in range(0, 9, 3):
-    #         box.append(board[i][j:j+3] + board[i+1][j:j+3] + board[i+2][j:j+3])
-    # l='.'
-    # for k in box:
-    #     while l in k:
+    for i in range(0, 9, 3):
+        for j in range(0, 9, 3):
+            box.append(board[i][j:j+3] + board[i+1][j:j+3] + board[i+2][j:j+3])
+    l = '.'
+    for k in box:
+        while l in k:
 
-    #         k.remove(l)
+            k.remove(l)
 
-    # for i in range(1,10):
-    #     row[i]={}
-    #     column[i]={}
+    for i in range(1, 10):
+        row[i] = {}
+        column[i] = {}
 
-    # for i in range(1,10):
-    #     for j in range(1,10):
-    #         row[i][j]=0
-    #         column[i][j]=0
+    for i in range(1, 10):
+        for j in range(1, 10):
+            row[i][j] = 0
+            column[i][j] = 0
 
-    # #print (row)
-    # for i in range(9):
-    #     for j in range(9):
-    #         cur=board[i][j]
-    #         if cur !='.':
-    #             cur=int(cur)
-    #             # for row
-    #             if cur in row[i+1] and row[i+1][cur]==1:
-    #                 return False
-    #             if cur in row[i+1] and row[i+1][cur]==0:
-    #                 row[i+1][cur]+=1
+    #print (row)
+    for i in range(9):
+        for j in range(9):
+            cur = board[i][j]
+            if cur != '.':
+                cur = int(cur)
+                # for row
+                if cur in row[i+1] and row[i+1][cur] == 1:
+                    return False
+                if cur in row[i+1] and row[i+1][cur] == 0:
+                    row[i+1][cur] += 1
 
-    #             # for column
-    #             if cur in column[j+1] and column[j+1][cur]==1:
-    #                 return False
-    #             if cur in column[j+1] and column[j+1][cur]==0:
-    #                 column[j+1][cur]+=1
-    # count=0
-    # for i in range(1,10):
-    #     for j in range(1,10):
-    #         if row[i][j]!=0:
-    #             count+=1
-    # if count==0: return True
+                # for column
+                if cur in column[j+1] and column[j+1][cur] == 1:
+                    return False
+                if cur in column[j+1] and column[j+1][cur] == 0:
+                    column[j+1][cur] += 1
+    count = 0
+    for i in range(1, 10):
+        for j in range(1, 10):
+            if row[i][j] != 0:
+                count += 1
+    if count == 0:
+        return True
 
-    # for m in box:
-    #     if len(m)-len(set(m))!=0:
-    #         return False
+    for m in box:
+        if len(m)-len(set(m)) != 0:
+            return False
 
-    # return True
+    return True
+
+
+def isValidSudoku2(board):
 
     rows = []
     # * Checking row
