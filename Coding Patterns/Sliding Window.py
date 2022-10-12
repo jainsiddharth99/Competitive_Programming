3ba285a45a16f7137ac214ccc51caef3ef6f4757
jainsiddharth99 <groupofsjain@gmail.com>
2022-10-12 14:45:56 +0530

    solved

--- Coding Patterns/Sliding Window.py
+++ Coding Patterns/Sliding Window.py
@@ -17,6 +17,20 @@ k = 2
 print(SlidingWindow(nums, k))
 
 
-def DynamicSlidingWindow(nums):
-    maxx = float('-inf')
-    return maxx
+def DynamicSlidingWindow(nums, total):
+    minn = float('inf')
+    s = 0
+    e = 0
+    curr = 0
+    while e < len(nums):
+        curr += nums[e]
+        e += 1
+        while s < e and curr >= total:
+            curr -= nums[s]
+            s += 1
+            minn = min(minn, e-s+1)
+    return minn
+
+
+nums = [1, 2, 3, 4, 5, 9, 12, 22, 11]
+print(DynamicSlidingWindow(nums, 30))
