01f5b2c492f634313d1a248c68168e06e1e0b071
jainsiddharth99 <groupofsjain@gmail.com>
2022-10-14 14:43:57 +0530

    solved

---
+++ LeetCode/424. Longest Repeating Character Replacement.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l, r, maxx = 0, 0, 0
+        cnt = Counter()
+        while r < len(s):
+            cnt[s[r]] += 1
+
+            if (r-l+1) - max(cnt.values()) <= k:
+                maxx = max(maxx, r-l+1)
+            else:
+                cnt[s[l]] -= 1
+                if not cnt[s[l]]:
+                    cnt.pop(s[l])
+                l += 1
+
+            r += 1
+        return len(s)-l
