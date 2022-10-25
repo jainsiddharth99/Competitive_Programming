a5cbb4aba5a9a346f77bca1b0a15848137eef3ba
jainsiddharth99 <groupofsjain@gmail.com>
2022-10-25 13:19:38 +0530

    tree

--- Data Structures and Algorithms/Tree.py
+++ Data Structures and Algorithms/Tree.py
@@ -1,9 +1,55 @@
-class Treenode:
+class TreeNode:
     def __init__(self, data):
         self.data = data
         self.children = []
         self.parent = None
 
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
+
+    def get_level(self):
+        l = 0
+        p = self.parent
+        while p:
+            l += 1
+            p = p.parent
+        return l
+
+    def print_tree(self):
+        spaces = " "*self.get_level()*3
+        pre = spaces+'|__' if self.parent else ""
+        print(pre+self.data)
+        if self.children:
+            for child in self.children:
+                child.print_tree()
+
+
+def build_tree():
+    root = TreeNode("Electronics")
+
+    laptop = TreeNode("Laptop")
+    laptop.add_child(TreeNode("Mac"))
+    laptop.add_child(TreeNode("Surface"))
+    laptop.add_child(TreeNode("Thinkpad"))
+
+    cellphone = TreeNode("Cell Phone")
+    cellphone.add_child(TreeNode("iPhone"))
+    cellphone.add_child(TreeNode("Google Pixel"))
+    cellphone.add_child(TreeNode("Vivo"))
+
+    tv = TreeNode("TV")
+    tv.add_child(TreeNode("Samsung"))
+    tv.add_child(TreeNode("LG"))
+
+    root.add_child(laptop)
+    root.add_child(cellphone)
+    root.add_child(tv)
+
+    return root
+
 
 if __name__ == "__main__":
+    root = build_tree()
+    root.print_tree()
     pass
